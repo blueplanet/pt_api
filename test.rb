d1e64fde1799d4df15467a3ec91707060c9626d4
blueplanet <erguolinge@gmail.com>
2016-05-22 20:22:57 +0900

    feature/118686183_story_name re #118686183

--- test.rb
+++ test.rb
@@ -1,32 +1,43 @@
-#! /bin/ruby
 require 'net/http'
+require 'json'
 
-class PivotalTracker
-  PROJECT_ID = 'xxxx'
-  PULL_REQUEST_TEMPLATE = '.pull_requestf_message'
+PULL_REQUEST_TEMPLATE = '.pull_requestf_message'
 
-  def initialize
-    current_branch = `git symbolic-ref --short HEAD`
-    @story_id = current_branch.scan(/(\d+)/).first
+def get_story_id
+  current_branch = `git symbolic-ref --short HEAD`
+  current_branch.match(/(\d+)/)
+  $1
+end
+
+def get_story_name(story_id)
+  story_name = nil
+
+  url = URI.parse("https://www.pivotaltracker.com/services/v5/projects/#{ENV['PT_PROJECT_ID']}/stories/#{story_id}")
+  https = Net::HTTP.new(url.host, 443)
+  https.use_ssl = true
+  result = JSON(https.get(url.path, 'X-TrackerToken' => ENV['PT_TOKEN']).body)
+  story_name = result['name']
+end
+
+def write_pull_request_template(story_id, story_name)
+  open(File.join(Dir.pwd, '.git', PULL_REQUEST_TEMPLATE), 'w') do |file|
+    file.puts "[fixed ##{story_id}]#{story_name}"
+    file.puts "\n"
+    file.puts "https://www.pivotaltracker.com/story/show/#{story_id}"
   end
+end
 
-  def story_name(story_id)
-    story_name = ''
+story_id = get_story_id
+puts "story_id: #{story_id}"
+if story_id
+  story_name = get_story_name(story_id)
 
-    url = URI.parse("https://www.pivotaltracker.com/services/v5/projects/#{project_id}/stories/#{story_id}")
-    https = Net::HTTP.new(url.host, 443)
-    https.use_ssl = true
-    result = JSON(https.get(url.path, 'X-TrackerToken' => ENV['PT_TOKEN']).body)
-    story_name = result['name']
-  rescue
-    nil
+  if story_name
+    puts "story_name: #{story_name}"
+    write_pull_request_template story_id, story_name
+    # ihub pull-request`
+    # `hub browse`
   end
+else
 
-  def write_pull_request_template
-    open(File.join(File.pwd, '.git', PULL_REQUEST_TEMPLATE), 'w') do |file|
-      file.puts "[fixed ##{story_id}]#{story_name}"
-      file.puts "\n"
-      file.puts "https://www.pivotaltracker.com/story/show/#{story_id}"
-    end
-  end
 end
