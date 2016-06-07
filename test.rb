ac8924d2d2fecddffc8dfcfa374699b2df879953
blueplanet <erguolinge@gmail.com>
2016-06-07 11:22:20 +0900

    fix story name

--- test.rb
+++ test.rb
@@ -10,14 +10,12 @@ def get_story_id
 end
 
 def get_story_name(story_id)
-  story_name = nil
-
   url = URI.parse("https://www.pivotaltracker.com/services/v5/projects/#{ENV['PT_PROJECT_ID']}/stories/#{story_id}")
   https = Net::HTTP.new(url.host, 443)
   https.use_ssl = true
-  result = JSON(https.get(url.path, 'X-TrackerToken' => ENV['PT_TOKEN']).body)
-  puts result
-  story_name = result['name']
+
+  response = https.get(url.path, 'X-TrackerToken' => ENV['PT_TOKEN'])
+  response.status_code == 200 ? JSON(response.body)['name'] : nil
 end
 
 def write_pull_request_template(story_id, story_name)
@@ -35,11 +33,8 @@ end
 story_id = get_story_id
 if story_id
   story_name = get_story_name(story_id)
-  write_pull_request_template story_id, 'test'
 
-  if story_name
-    write_pull_request_template story_id, story_name
-  end
+  write_pull_request_template story_id, story_name if story_name
 end
 
 system 'hub pull-request --browse'
